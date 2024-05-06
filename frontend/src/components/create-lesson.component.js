56eb1d99e8b4730c04850a8d28bfc1f4eeb0239a
Yin <yxl21@ic.ac.uk>
2024-05-06 19:56:31 +0100

    lesson objectives styling

--- frontend/src/components/create-lesson.component.js
+++ frontend/src/components/create-lesson.component.js
@@ -2,6 +2,8 @@ import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import AuthContext from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import * as AiIcons from "react-icons/ai";
+import { IoAddOutline } from "react-icons/io5";
 // import validator from 'validator'
 import "../styles/Lesson.css";
 
@@ -23,6 +25,7 @@ export default function CreateLesson() {
 
   const [title, setTitle] = useState('');
   const [lessonObjectivesList, setLessonObjectives] = useState([{lessonObjective: ""}])
+
   const [lesson, setLesson] = useState({
         title: useState(''),
         author: '',
@@ -125,37 +128,41 @@ export default function CreateLesson() {
         <br/>
 
         <div className="form-group"> 
-          <label className="input-label">Lesson Objectives: </label>
+        <label className="input-label objectives-label">
+            <p>Lesson Objectives: </p>
+            <button type="button" onClick={addLessonObjective} className="add-btn"> 
+              <IoAddOutline />
+            </button>
+        </label>
+
+
           {lessonObjectivesList.map((singleLessonObjective, index) => (
-            <div key={index} className="objectives">
-              <div className="first-division">
-                {lessonObjectivesList.length - 1 === index && lessonObjectivesList.length < 4 && (
-                  <button type="button" onClick={addLessonObjective} className="add-btn"> 
-                    Add Lesson Objective 
-                  </button>
-                )}
-                <input
-                  name="service"
-                  type="text"
-                  id="service"
-                  value={singleLessonObjective.lessonObjective}
-                  onChange={(e) => onChangeLessonObjectives(e, index)}
-                  required
-                />
-              </div>
-              <div className="second-division">
-                {lessonObjectivesList.length !== 1 && (
-                  <button
-                    type="button"
-                    onClick={() => deleteLessonObjective(index)}
-                    className="remove-btn"
-                  >
-                    <span>Remove</span>
-                  </button>
-                )}
-              </div>
+          <div key={index} className="objectives">
+            <div className="lesson-row">
+              <input
+                name="lessonObjective"
+                type="text"
+                id={`service-${index}`} // Use a unique ID for each input field
+                value={singleLessonObjective.lessonObjective}
+                onChange={(e) => onChangeLessonObjectives(e, index)}
+                className="input-field transparent-input"
+              />
+              {/* {lessonObjectivesList.length !== 1 && ( */}
+                <button
+                  type="button"
+                  onClick={() => deleteLessonObjective(index)}
+                  className="remove-btn"
+                >
+                  <AiIcons.AiOutlineClose />
+                </button>
+              {/* )} */}
             </div>
-          ))}
+            {/* {lessonObjectivesList.length - 1 === index && lessonObjectivesList.length < 4 && (
+              
+            )} */}
+          </div>
+        ))}
+
         </div>
         
   
