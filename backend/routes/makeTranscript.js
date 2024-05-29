ea44e0327c0f96606e0140dc8a7001059209fa97
Yin <yxl21@ic.ac.uk>
2024-05-29 11:09:28 +0100

    download mp3 and conversion works

--- backend/routes/makeTranscript.js
+++ backend/routes/makeTranscript.js
@@ -13,110 +13,78 @@ const path = require('path');
 const client = new speech.SpeechClient();
 const storage = new Storage();
 
-async function youtubeToMP3(youtubeID){
-    let stream = ytdl(id, {
+async function youtubeToMP3(title, youtubeID){
+    let stream = ytdl(youtubeID, {
       quality: 'highestaudio',
     });
     
+    const filePath = `${__dirname}/audioMP3/${title}.mp3`; 
     let start = Date.now();
     ffmpeg(stream)
       .audioBitrate(128)
-      .save(`${__dirname}/${youtubeID}.mp3`)
+      .save(filePath)
       .on('progress', p => {
         readline.cursorTo(process.stdout, 0);
         process.stdout.write(`${p.targetSize}kb downloaded`);
-      })
-      .on('end', () => {
+    })
+    .on('end', () => {
         console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
       });
-  }
-
-async function downloadMP3(url, filename) {
-    console.log("MP3 url:", url);
-    console.log("Filename:", filename);
-
-    const directoryPath = './audioMP3';
-    const filePath = path.join(directoryPath, `${filename}.mp3`);
-
-    try {
-        const response = await axios.get(url, { responseType: 'arraybuffer' });
-        fs.writeFileSync(filePath, response.data);
-        console.log(fs.writeFileSync(filePath, response.data));
-        return filePath;
-    } catch (error) {
-        console.error('Error downloading MP3:', error);
-        throw error;
-    }
+    return filePath;
 }
 
-async function convertMP3ToWAV(mp3Path, wavPath) {
-    return new Promise((resolve, reject) => {
-        ffmpeg(mp3Path)
-            .toFormat('wav')
-            .audioCodec('pcm_s16le')
-            .on('end', resolve)
-            .on('error', reject)
-            .save(wavPath);
-    });
-}
-
-async function uploadToGCS(localPath, bucketName, destination) {
-    await storage.bucket(bucketName).upload(localPath, {
-        destination: destination,
-    });
-    console.log(`${localPath} uploaded to ${bucketName}/${destination}`);
-}
-
-async function transcribeMP3(mp3Url) {
-    try {
-        const audio = { 
-        uri: mp3Url 
+async function transcribeAudio(title, mp3File) {
+    const output = `${__dirname}/audioWAV/${title}.wav`; 
+  
+    // Convert MP3 to WAV
+    ffmpeg(mp3File)
+      .toFormat('wav')
+      .on('error', (err) => {
+        console.error('Error converting file:', err);
+      })
+      .on('end', async () => {
+        console.log('File has been converted successfully');
+  
+        const file = fs.readFileSync(output);
+        const audioBytes = file.toString('base64');
+  
+        const audio = {
+          content: audioBytes,
         };
+  
         const config = {
-            encoding: 'MP3',
-            sampleRateHertz: 44100, 
-            languageCode: 'en-US'
+          encoding: 'LINEAR16',
+          sampleRateHertz: 16000,
+          languageCode: 'en-US',
         };
-        const request = { 
-            audio: audio, 
-            config: config 
+  
+        const request = {
+          audio: audio,
+          config: config,
         };
+  
+        // Detects speech in the audio file
         const [response] = await client.recognize(request);
         const transcription = response.results
-            .map(result => result.alternatives[0].transcript)
-            .join('\n');
-        console.log(`Transcription: ${transcription}`);
-    return transcription;
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
+          .map(result => result.alternatives[0].transcript)
+          .join('\n');
+        console.log('Transcription: ', transcription);
+      })
+      .save(output);
+};
 
 
 
 
 async function makeTranscript(title, youtubeID) {
-    const mp3Url = await youtubeToMP3(youtubeID);
-    console.log(mp3Url);
-
-    // const sanitizedTitle = title.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+    const sanitizedTitle = title.replace(/[^a-z0-9]/gi, '_').toLowerCase();
     // console.log(sanitizedTitle); 
 
-    // const mp3Path = `./audioMP3/${sanitizedTitle}.mp3`;
-    // const wavPath = `./audioMP3/${sanitizedTitle}.wav`;
-    // const bucketName = 'audioMP3';
-    // const gcsUri = `gs://${bucketName}/${sanitizedTitle}}.wav`;
-
     try {
-        ytToMP3(); 
-        // await downloadMP3(mp3Url, sanitizedTitle)
-        //     .then(() => console.log('Download completed'))
-        //     .catch(error => console.error('Error downloading MP3:', error));
-        // await convertMP3ToWAV(mp3Path, wavPath)
-        //     .then(() => console.log('Conversion to wav completed'))
-        //     .catch(error => console.error('Error converting to wav:', error));
-        // await uploadToGCS(wavPath, bucketName, `${title}.wav`);
-        // await generateTranscript(gcsUri); 
+        const mp3Path = await youtubeToMP3(sanitizedTitle, youtubeID);
+        console.log(mp3Path);
+
+        await transcribeAudio(sanitizedTitle, mp3Path); 
     } catch (error) {
         console.error('Error:', error);
     }
